35e09a85a4f5b673a8c42350da626b8dccb7be46
Keith Dahlby <dahlbyk@gmail.com>
2011-01-06 19:47:28 -0600

    Add FSRazorViewEngine

---
+++ src/FSRazor.Mvc/FSRazorViewEngine.fs
@@ -0,0 +1,21 @@
+﻿namespace FSRazor
+
+open Microsoft.FSharp.Collections
+open System.Web.Mvc
+
+type FSRazorViewEngine() =
+    inherit RazorViewEngine()
+
+    let extensions = [| "fshtml"; "cshtml"; "vbhtml" |]
+    let appendExtensions p = extensions |> Array.map (fun e -> p + e)
+    let areaLocationFormats = [| "~/Areas/{2}/Views/{1}/{0}."; "~/Areas/{2}/Views/Shared/{0}." |]
+    let locationFormats = [| "~/Views/{1}/{0}."; "~/Views/Shared/{0}." |]
+
+    do
+        base.FileExtensions <- extensions
+        base.AreaViewLocationFormats <- Array.collect appendExtensions areaLocationFormats
+        base.AreaMasterLocationFormats <- Array.collect appendExtensions areaLocationFormats
+        base.AreaPartialViewLocationFormats <- Array.collect appendExtensions areaLocationFormats
+        base.ViewLocationFormats <- Array.collect appendExtensions locationFormats
+        base.MasterLocationFormats <- Array.collect appendExtensions locationFormats
+        base.PartialViewLocationFormats <- Array.collect appendExtensions locationFormats
