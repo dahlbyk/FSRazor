d21f62eb45cb2ac2ad66408e53851db538e1fc6c
Keith Dahlby <dahlbyk@gmail.com>
2011-01-06 20:37:29 -0600

    Add sample MVC application

---
+++ src/FSRazor.Sample.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace FSRazor.Sample.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to FSRazor";
+            return View();
+        }
+    }
+}
