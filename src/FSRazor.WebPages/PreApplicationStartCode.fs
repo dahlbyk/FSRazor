ace922273027ab54cbbb9b5a1ac700cda543a1d2
Keith Dahlby <dahlbyk@gmail.com>
2011-02-05 10:49:29 -0600

    Add FSRazor.WebPages

---
+++ src/FSRazor.WebPages/PreApplicationStartCode.fs
@@ -0,0 +1,17 @@
+﻿namespace FSRazor.WebPages
+
+open System
+open System.ComponentModel
+open System.Web.WebPages
+
+[<EditorBrowsable(EditorBrowsableState.Never)>]
+type PreApplicationStartCode() =
+    static let mutable startWasCalled = false
+
+    static member Start() =
+        if (not startWasCalled) then
+            startWasCalled <- true
+
+            FSRazor.PreApplicationStartCode.Start()
+
+            WebPageHttpHandler.RegisterExtension("fshtml")
